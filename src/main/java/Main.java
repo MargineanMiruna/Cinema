ad3411c4a26dfbca09feea362dc855278da7cff2
Miru <miruna.marginean.2018@gmail.com>
2024-12-11 13:04:34 +0200

    project structure modifications

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,37 +16,37 @@ import java.time.LocalTime;
 
 class Main {
     public static void main(String[] args) {
-        Connection connection;
-        final String DB_URL = "jdbc:sqlite:" + System.getProperty("user.dir") + "/src/main/java/Files/cinemaDB.db";
+//        Connection connection;
+//        final String DB_URL = "jdbc:sqlite:" + System.getProperty("user.dir") + "/src/main/java/Files/cinemaDB.db";
+//
+//        try {
+//            connection = DriverManager.getConnection(DB_URL, "user", "password");
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//       // Database Repository
+//        IRepository<Customer> customerRepo = new CustomerDBRepository(connection);
+//        IRepository<Staff> staffRepo = new StaffDBRepository(connection);
+//        IRepository<Booking> bookingRepo = new BookingDBRepository(connection);
+//        IRepository<Showtime> showtimeRepo = new ShowtimeDBRepository(connection);
+//        IRepository<Movie> movieRepo = new MovieDBRepository(connection);
+//        IRepository<Screen> screenRepo = new ScreenDBRepository(connection);
+//        IRepository<Seat> seatRepo = new SeatDBRepository(connection);
+//        IRepository<Ticket> ticketRepo = new TicketDBRepository(connection);
+//        IRepository<BasicMembership> basicMembershipRepo = new BasicMembershipDBRepository(connection);
+//        IRepository<PremiumMembership> premiumMembershipRepo = new PremiumMembershipDBRepository(connection);
 
-        try {
-            connection = DriverManager.getConnection(DB_URL, "user", "password");
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-       // Database Repository
-        IRepository<Customer> customerRepo = new CustomerDBRepository(connection);
-        IRepository<Staff> staffRepo = new StaffDBRepository(connection);
-        IRepository<Booking> bookingRepo = new BookingDBRepository(connection);
-        IRepository<Showtime> showtimeRepo = new ShowtimeDBRepository(connection);
-        IRepository<Movie> movieRepo = new MovieDBRepository(connection);
-        IRepository<Screen> screenRepo = new ScreenDBRepository(connection);
-        IRepository<Seat> seatRepo = new SeatDBRepository(connection);
-        IRepository<Ticket> ticketRepo = new TicketDBRepository(connection);
-        IRepository<BasicMembership> basicMembershipRepo = new BasicMembershipDBRepository(connection);
-        IRepository<PremiumMembership> premiumMembershipRepo = new PremiumMembershipDBRepository(connection);
-
-//        //File Repository
-//        IRepository<Customer> customerRepo = new FileRepository<>(System.getProperty("user.dir") + "\\src\\Files\\customer.csv", Customer::fromCSV);
-//        IRepository<Staff> staffRepo = new FileRepository<>(System.getProperty("user.dir") + "\\src\\Files\\staff.csv", Staff::fromCSV);
-//        IRepository<Booking> bookingRepo = new FileRepository<>(System.getProperty("user.dir") + "\\src\\Files\\booking.csv", Booking::fromCSV);
-//        IRepository<Showtime> showtimeRepo = new FileRepository<>(System.getProperty("user.dir") + "\\src\\Files\\showtime.csv", Showtime::fromCSV);
-//        IRepository<Movie> movieRepo = new FileRepository<>(System.getProperty("user.dir") + "\\src\\Files\\movie.csv", Movie::fromCSV);
-//        IRepository<Screen> screenRepo = new FileRepository<>(System.getProperty("user.dir") + "\\src\\Files\\screen.csv", Screen::fromCSV);
-//        IRepository<Seat> seatRepo = new FileRepository<>(System.getProperty("user.dir") + "\\src\\Files\\seat.csv", Seat::fromCSV);
-//        IRepository<Ticket> ticketRepo = new FileRepository<>(System.getProperty("user.dir") + "\\src\\Files\\ticket.csv", Ticket::fromCSV);
-//        IRepository<BasicMembership> basicMembershipRepo = new FileRepository<>(System.getProperty("user.dir") + "\\src\\Files\\basicMembership.csv", BasicMembership::fromCSV);
-//        IRepository<PremiumMembership> premiumMembershipRepo = new FileRepository<>(System.getProperty("user.dir") + "\\src\\Files\\premiumMembership.csv", PremiumMembership::fromCSV);
+        //File Repository
+        IRepository<Customer> customerRepo = new FileRepository<>(System.getProperty("user.dir") + "\\src\\main\\java\\Files\\customer.csv", Customer::fromCSV);
+        IRepository<Staff> staffRepo = new FileRepository<>(System.getProperty("user.dir") + "\\src\\main\\java\\Files\\staff.csv", Staff::fromCSV);
+        IRepository<Booking> bookingRepo = new FileRepository<>(System.getProperty("user.dir") + "\\src\\main\\java\\Files\\booking.csv", Booking::fromCSV);
+        IRepository<Showtime> showtimeRepo = new FileRepository<>(System.getProperty("user.dir") + "\\src\\main\\java\\Files\\showtime.csv", Showtime::fromCSV);
+        IRepository<Movie> movieRepo = new FileRepository<>(System.getProperty("user.dir") + "\\src\\main\\java\\Files\\movie.csv", Movie::fromCSV);
+        IRepository<Screen> screenRepo = new FileRepository<>(System.getProperty("user.dir") + "\\src\\main\\java\\Files\\screen.csv", Screen::fromCSV);
+        IRepository<Seat> seatRepo = new FileRepository<>(System.getProperty("user.dir") + "\\src\\main\\java\\Files\\seat.csv", Seat::fromCSV);
+        IRepository<Ticket> ticketRepo = new FileRepository<>(System.getProperty("user.dir") + "\\src\\main\\java\\Files\\ticket.csv", Ticket::fromCSV);
+        IRepository<BasicMembership> basicMembershipRepo = new FileRepository<>(System.getProperty("user.dir") + "\\src\\main\\java\\Files\\basicMembership.csv", BasicMembership::fromCSV);
+        IRepository<PremiumMembership> premiumMembershipRepo = new FileRepository<>(System.getProperty("user.dir") + "\\src\\main\\java\\Files\\premiumMembership.csv", PremiumMembership::fromCSV);
 
 
         //In-Memory Repository
